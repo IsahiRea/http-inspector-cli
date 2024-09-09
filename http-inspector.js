86a2787a5b0c0aefbf7f3b000f29394fe77d7c60
IsahiRea <Isahi.rea@live.com>
2024-09-09 18:35:56 -0500

    Add CM options and display response using chalk

--- http-inspector.js
+++ http-inspector.js
@@ -14,6 +14,11 @@ const makeRequest = async (method, url, headers, data) => {
             headers: headers ? JSON.parse(headers) : undefined,
             data: data ? JSON.parse(data) : undefined,
         });
+
+        console.log(chalk.greenBright(`Status: ${response.status}`));
+        console.log(chalk.yellow('Headers:'), response.headers);
+        console.log(chalk.yellow('Response Data:'));
+        console.log(chalk.blueBright(JSON.stringify(response.data, null, 2)));
         
     } catch (error) {
         console.error(`Error: ${error.message}`);
@@ -36,14 +41,29 @@ program
 program
     .command('post <url>')
     .description('Send a POST request to a URL ')
+    .option('-H, --headers <headers>', 'Custom headers in JSON format')
+    .option('-D, --data <data>', 'Custom data in JSON format')
+    .action((url, options) => {
+        makeRequest('post', url, options.headers, options.data);
+    })
 
 program
     .command('put <url>')
     .description('Send a PUT request to a URL')
+    .option('-H, --headers <headers>', 'Custom headers in JSON format')
+    .option('-D, --data <data>', 'Custom data in JSON format')
+    .action((url, options) => {
+        makeRequest('put', url, options.headers, options.data);
+    })
+    
 
 program
     .command('delete <url>')
     .description('Send a DELETE request to a URL ')
+    .option('-H, --headers <headers>', 'Custom headers in JSON format')
+    .action((url, options) => {
+        makeRequest('delete', url, options.headers, null);
+    })
 
 // Parse the CLI arguments
 program.parse(process.argv);
