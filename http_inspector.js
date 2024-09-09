dbb7c033e08c80889a39d0b97ce6216c14fafa85
IsahiRea <Isahi.rea@live.com>
2024-09-08 22:43:39 -0500

    Base Setup

---
+++ http_inspector.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const {program} = require('commander');
+const axios = require('axios');
+const chalk = require('chalk');
+
+// Create a function to handle requests
+const makeRequest = async (method, url, headers, data) => {
+    
+    try {
+        const response = await axios({
+
+        })
+        
+    } catch (error) {
+        
+    }
+}
+
+
+program
+    .version('1.0.0')
+    .description('HTTP Request Inspector CLI')
+
+program
+    .command('get <url>')
+    .description('Send a GET request to a URL')
+    .option('-H, --headers <headers>', 'Custom headers in JSON format')
+    .action((url, options) => {
+        makeRequest('get', url, options.headers, null);
+    })
+
+program
+    .command('post <url>')
+    .description('Send a POST request to a URL ')
+
+program
+    .command('put <url>')
+    .description('Send a PUT request to a URL')
+
+program
+    .command('delete <url>')
+    .description('Send a DELETE request to a URL ')
+
+// Parse the CLI arguments
+program.parse(process.argv);
